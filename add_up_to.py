ecde2fd5fbe4aa59fcf811eafe92e31a5d761253
Ian <exquistian@gmail.com>
2020-07-28 16:34:23 -0700

    first problem upload

---
+++ add_up_to.py
@@ -0,0 +1,15 @@
+import pdb
+
+def add_up_to(the_l, k):
+	placeholder = [-1] * len(the_l)
+	for i in range(len(the_l)):
+		if the_l[i] <= k:
+			placeholder[i] = k - the_l[i]
+		if i > 0:
+			for x in range(i):
+				if the_l[i] == placeholder[x] and placeholder[x] != -1:
+					return True
+	return False
+
+test = [7, 16, 3, 11, 100]
+print(add_up_to(test, 17))
